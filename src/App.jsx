056e50a6dc01eb8dc82c3820ac9e53fef20ef8a7
AndreaCatapano <a.catapano96@gmail.com>
2025-04-11 16:43:15 +0200

    feat: added a button for choise api to view in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-
-
 import ActorCard from './components/ActorCard.jsx'
 
-
-
-
 function App() {
-
-  const [actors, setActresses] = useState([])
-
+  const [actors, setActors] = useState([])
   const [url, setUrl] = useState("https://www.freetestapi.com/api/v1/actresses")
+  const [activeTab, setActiveTab] = useState("actresses")
 
-  function getActress() {
-    const response = axios.get(url).then((res) => setActresses(res.data))
+  function fetchActor() {
+    axios.get(url).then((res) => setActors(res.data))
       .catch((err => console.log(err)))
   }
 
-  useEffect(() => getActress(), []);
+  useEffect(() => fetchActor(), [url]);
+
+  const showActresses = () => {
+    setUrl("https://www.freetestapi.com/api/v1/actresses")
+    setActiveTab("actresses")
+  }
+
+  const showActors = () => {
+    setUrl("https://www.freetestapi.com/api/v1/actors")
+    setActiveTab("actors")
+  }
 
   return (
     <>
+      <div className="toggle-container">
+        <button
+          className={`toggle-button ${activeTab === "actresses" ? "active" : ""}`}
+          onClick={showActresses}>  Actresses  </button>
+        <button
+          className={`toggle-button ${activeTab === "actors" ? "active" : ""}`}
+          onClick={showActors}> Actors </button>
+      </div>
+
       <div className="cards-container">
         {actors.map(actor => (
           <ActorCard key={actor.id} actor={actor} />
